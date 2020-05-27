cbcd4ef85c44254cc986badedce3a655a07dc75c
Luke Greenberg <lukegreen.pc@gmail.com>
2020-05-27 18:27:33 +0200

    Project init

---
+++ objectid/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from objectid.users.models import User
+from objectid.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
