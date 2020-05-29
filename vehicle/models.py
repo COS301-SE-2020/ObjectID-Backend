24f1fb6c60d7accb6fc12f44794f3309b1c136f2
Luke Greenberg <lukegreen.pc@gmail.com>
2020-05-29 18:00:20 +0200

    #5gpcc4 add basic models for vehicles

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehicle(models.Model):
+    licence_plate = models.CharField(max_length=10)
+    make = models.CharField(max_length=64)
+    model = models.CharField(max_length=64)
+    color = models.CharField(max_length=32) # TODO: Consider making this a Foreign key for set values?
+
+    def __str__(self):
+        return "{}: {} {}".format(self.model, self.licence_plate, self.color)        
+
+class DamageLocation(models.Model):
+    location = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.location
+
+class DamageType(models.Model):
+    description = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.description
+
+class Damage(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='damage')
+    location = models.ForeignKey(DamageLocation, on_delete=models.CASCADE, related_name='damage')
+    type = models.ForeignKey(DamageType, on_delete=models.CASCADE, related_name='damage')
+
+    def __str__(self):
+        return "{}: {} at {}".format(self.vehicle.licence_plate, self.type.description, self.location.location)
