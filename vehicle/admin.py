c92be83ab95f42e42ec8fec22a5262d7c1b4bc89
Luke Greenberg <lukegreen.pc@gmail.com>
2020-06-09 16:18:50 +0200

    add admin

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Vehicle, Damage, DamageLocation, DamageType
+
 # Register your models here.
+
+admin.site.register(Vehicle)
+admin.site.register(Damage)
+admin.site.register(DamageLocation)
+admin.site.register(DamageType)
