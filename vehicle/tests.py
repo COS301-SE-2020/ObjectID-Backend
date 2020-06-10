eb38de287ce26c6a3461233e37809adcee991e32
Stephen Du Plessis <u17049203@tuks.co.za>
2020-06-10 17:27:30 +0200

    unitTesting update

--- vehicle/tests.py
+++ vehicle/tests.py
@@ -1,5 +1,7 @@
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
+import requests
+
 # Create your tests here.
 class VehicleTests(APITestCase):
     def test_add_vehicle_basic(self):
@@ -23,9 +25,9 @@ class VehicleTests(APITestCase):
         response = self.client.post(url,data)
         self.assertEqual(response.status_code,200)
 
-    def test_and_search(self):
+    def test_or_search(self):
 
-        url = '/api/v1/vehicle/and_search/'
+        url = '/api/v1/vehicle/search_or/'
         data = {
             'filters' : {
             'license_plate' : 'BE32SNGP',
@@ -37,16 +39,27 @@ class VehicleTests(APITestCase):
         response = self.client.post(url,data, format='json')
         self.assertEqual(response.status_code,200)
 
-    # def test_file_recognize(self):
+    def test_file_recognize(self):
+        import pathlib
+        url = '/api/v1/vehicle/file_recognize/'
+        
 
-    #     url = '/api/v1/vehicle/file_recognize/'
-    #     data = {
-    #         'license_plate' : 'BE32SNGP',
-    #         'make' : 'Toyota',
-    #         'model' : 'Corolla',
-    #         'color' : 'White'
-    #     }
-    #     response = self.client.post(url,data)
-    #     self.assertEqual(response.status_code,200)
 
-    
+        # response = self.client.post(url,data)
+        
+
+        path = pathlib.Path(__file__).parent.absolute()
+
+        actual_path ='{}/test_images/2015-BMW-320d-xDrive-Touring-test-drive-67.jpg'.format(path)
+
+        files = [
+        ('file', open("{}".format(actual_path), 'rb'))
+        ]
+        data = {
+             'file' : files[0]
+        }
+
+        response = self.client.post(url, data=data, files=files)
+
+
+        self.assertEqual(response.status_code, 200)
