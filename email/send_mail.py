5821c4a63239e11cac02581235ebd69b5b51a91f
JohanWol <u17043540@tuks.co.za>
2020-07-30 21:12:43 +0200

    Added multiple messages types to mail

--- email/send_mail.py
+++ email/send_mail.py
@@ -1,6 +1,10 @@
 import email_to
-server = email_to.EmailServer('smtp.gmail.com', 587, 'ctrl.intelligence@gmail.com', 'Ctrl+Intelligence312##')
 
-server.quick_email('haainks@gmail.com', 'Test',
-                    ['# A Heading', 'Something else in the body'],
-                   style='h1 {color: blue}')
+def send(address,type):
+    server = email_to.EmailServer('smtp.gmail.com', 587, 'ctrl.intelligence@gmail.com', 'Ctrl+Intelligence312##')
+
+    if type == 1:
+        server.quick_email(address,"Successful Registration",['Welcome ' + address +'.\n', 'You have successfully been registered on the ObjectID system you will now be able ' +
+                                                                    'to log in and use the functionalities of the system.\n', 'Thank you for using ObjectID.'])
+
+send("haainks@gmail.com",1)
