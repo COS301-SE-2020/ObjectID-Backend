9696966291353eaa3d88324a2149ef31a3b36ebb
JohanWol <u17043540@tuks.co.za>
2020-08-10 21:41:28 +0200

    :speech_balloon: updated email for flagged vehicle

--- custom_user/send_email.py
+++ custom_user/send_email.py
@@ -10,3 +10,8 @@ def send(address,type):
     elif type == 2:
         server.quick_email(address, "Account Removed", ['Greetings ' + address + '.\n', 'Your account has been suspended, and you will no longer be able to use the ObjectID system ' +
                                                                 'if you require any assistance please mail ctrl.intelligence@gmail.com.\n', 'Thank you for using ObjectID.'])
+
+def flagged_notification(address,lic_plate,flag,image,location, make, model,color):
+        server.quick_email(address, "FLAGGED VEHICLE SPOTTED", ["WARNING " + address+ '.\n', "A " + color+" "+ make+" "+model+" with the numberplate "+lic_plate+" has been spotted on a camera at "
+        + location + " which is in your area, our system shows that this vehicle is " + flag+"./n", "Please take the nesscary and report the vehicle to the authorities if it is spotted. A snapshot of the vehicle is "+
+        "attached to this email."])
