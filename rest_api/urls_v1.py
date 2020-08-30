04b27fb2cac699a54c0476a0e607e6ddb648e4d7
Luke Greenberg <lukegreen.dev@gmail.com>
2020-08-30 19:44:52 +0200

    expose camera endpoints

--- rest_api/urls_v1.py
+++ rest_api/urls_v1.py
@@ -3,9 +3,11 @@ from django.urls import path, re_path
 
 import vehicle.views as vehicle_viewset
 import custom_user.views as custom_user_viewset
+import camera.views as camera_viewset
 
 urlpatterns = [
     re_path(r'vehicle/(?P<action>[^/.]+)', vehicle_viewset.VehicleBase.as_view(), name='vehicle'),
     re_path(r'user/(?P<action>[^/.]+)', custom_user_viewset.CustomUserBase.as_view(), name='custom_user'),
     re_path(r'business/(?P<action>[^/.]+)', custom_user_viewset.BusinessBase.as_view(), name='business'),
+    re_path(r'camera/(?P<action>[^/.]+)', camera_viewset.CameraActions.as_view(), name='camera')
 ]
