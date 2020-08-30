7bca764e9694fc415bcfe932d4f4145e5aab9abe
Luke Greenberg <lukegreen.dev@gmail.com>
2020-08-30 15:04:28 +0200

    Add new migrations for model updates

--- custom_user/models.py
+++ custom_user/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 class Business(models.Model):
     related_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='business')
     name = models.CharField(max_length=64)
-    logo = models.ImageField(upload_to='business/logos/')
+    logo = models.ImageField(upload_to='business/logos/', blank=True, null=True, default=None)
 
     def __str__(self):
         return self.name
