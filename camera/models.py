e5efb01258192142118087ff05effcd0e518236c
Luke Greenberg <lukegreen.dev@gmail.com>
2020-08-30 15:04:37 +0200

    model updates

--- camera/models.py
+++ camera/models.py
@@ -9,6 +9,7 @@ class Camera(models.Model):
     lat = models.DecimalField(blank=True, max_digits=9, decimal_places=6, default=None, null=True)
     long = models.DecimalField(blank=True, max_digits=9, decimal_places=6, default=None, null=True)
     unique_key = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64)
 
     def __str__(self):
         return "Camera: {}".format(self.owner.business.name)
