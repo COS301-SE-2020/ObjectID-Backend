4ac3075dc085c1870900764c44f2b32a6abc3d8e
Luke Greenberg <lukegreen.dev@gmail.com>
2020-08-30 15:04:46 +0200

    add urls for new endpoints

--- rest_api/urls_v1.py
+++ rest_api/urls_v1.py
@@ -6,5 +6,6 @@ import custom_user.views as custom_user_viewset
 
 urlpatterns = [
     re_path(r'vehicle/(?P<action>[^/.]+)', vehicle_viewset.VehicleBase.as_view(), name='vehicle'),
-    re_path(r'user/(?P<action>[^/.]+)', custom_user_viewset.CustomUserBase.as_view(), name='custom_user')
+    re_path(r'user/(?P<action>[^/.]+)', custom_user_viewset.CustomUserBase.as_view(), name='custom_user'),
+    re_path(r'business/(?P<action>[^/.]+)', custom_user_viewset.BusinessBase.as_view(), name='business'),
 ]
